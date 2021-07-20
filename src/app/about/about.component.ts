7f378ec8ce0a8c97a5e0b6b996d85843a81789c8
Léo Vetter <vetter.leo@gmail.com>
2021-07-20 11:37:54 +0200

    store

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AsyncSubject, BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,85 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    // this.behaviorSubject();
+    // this.asyncSubject();
+    this.replaySUbject();
+  }
+
+  behaviorSubject() {
+
+    // Create a subject
+    const subject = new BehaviorSubject(0);
+
+    // Derive an observable from the subject
+    const series$ = subject.asObservable();
+
+    series$.subscribe(val => console.log('first sub : ' + val));
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+
+    subject.complete();
+
+    setTimeout(() => {
+
+      series$.subscribe(val => console.log('late sub : ' + val));
+
+      subject.next(4);
+
+    }, 3000)
+  }
+
+  asyncSubject() {
+
+    // Create a subject
+    const subject = new AsyncSubject();
+
+    // Derive an observable from the subject
+    const series$ = subject.asObservable();
+
+    series$.subscribe(val => console.log('first sub : ' + val));
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+
+    subject.complete();
+
+    setTimeout(() => {
+
+      series$.subscribe(val => console.log('second sub : ' + val));
+
+      // subject.next(4);
+
+    }, 3000)
+  }
+
+  replaySUbject() {
+
+    // Create a subject
+    const subject = new ReplaySubject();
+
+    // Derive an observable from the subject
+    const series$ = subject.asObservable();
+
+    series$.subscribe(val => console.log('first sub : ' + val));
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+
+    // subject.complete();
+
+    setTimeout(() => {
+
+      series$.subscribe(val => console.log('second sub : ' + val));
+
+      subject.next(4);
+
+    }, 3000)
   }
 
 }
